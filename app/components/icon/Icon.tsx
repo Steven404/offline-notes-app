e652273829623651b7ef130434697c541de00bbe
Stefanos Michelakis <stefanosmichelakis@gmail.com>
2026-01-30 19:28:09 +0200

    Add Icon component

---
+++ app/components/icon/Icon.tsx
@@ -0,0 +1,16 @@
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+type Props = {
+  name: IconProp;
+  size: number;
+  color: string;
+};
+
+const Icon = (props: Props) => {
+  const { name, size, color } = {
+    ...props,
+  };
+  return <FontAwesomeIcon icon={name} size={size} color={color} />;
+};
+
+export default Icon;
