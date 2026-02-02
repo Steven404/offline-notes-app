08223a4aaa384bff3461e8a89ac6e9263a00c817
Stefanos Michelakis <stefanosmichelakis@gmail.com>
2026-02-03 00:42:00 +0200

    Notes card and format date function

---
+++ app/features/notes/components/NoteCard.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import { Note } from '../NoteTypes.ts';
+import TextLabel from '../../../components/textLabel/TextLabel.tsx';
+import Colors from '../../../styles/colors.ts';
+import Fonts from '../../../styles/Fonts.tsx';
+import Icon from '../../../components/icon/Icon.tsx';
+import { formatDate } from '../../../utils/functions.ts';
+
+interface NoteCardProps {
+  note: Note;
+  onPress: () => void;
+}
+
+const NoteCard = ({ note, onPress }: NoteCardProps) => {
+  return (
+    <TouchableOpacity style={styles.container} onPress={onPress}>
+      <TextLabel text={note.title} style={styles.title} />
+      <View style={styles.lastEdited}>
+        <Icon name={'pencil'} size={12} color={Colors.placeholder} />
+        <TextLabel
+          text={formatDate(note.updatedAt)}
+          style={styles.lastEditedText}
+        />
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.tabBarBackground,
+    padding: 16,
+    borderRadius: 10,
+    marginBottom: 12,
+    maxHeight: 100,
+    overflow: 'hidden',
+  },
+  title: {
+    fontSize: 18,
+    fontFamily: Fonts.MontserratSemiBold,
+    color: Colors.white,
+    marginBottom: 8,
+  },
+  lastEdited: {
+    flexDirection: 'row',
+    marginTop: 2,
+    alignItems: 'center',
+    gap: 4,
+  },
+  lastEditedText: {
+    fontSize: 14,
+    fontFamily: Fonts.MontserratRegular,
+    color: Colors.placeholder,
+  },
+});
+
+export default NoteCard;
--- app/utils/functions.ts
+++ app/utils/functions.ts
@@ -13,3 +13,24 @@ export const sanitizeNoteContent = (content: string): string =>
   content.replace(/<li\b[^>]*>([\s\S]*?)<\/li>/gi, match =>
     match.replace(/<\/?h3\b[^>]*>/gi, ''),
   );
+
+/**
+ * Formats a timestamp into a dd/mm/yyyy, hh:mm AM/PM string.
+ * @param timestamp - The timestamp to format.
+ * @returns The formatted date string.
+ */
+export const formatDate = (timestamp: number): string => {
+  const date = new Date(timestamp);
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const year = date.getFullYear();
+  let hours = date.getHours();
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const ampm = hours >= 12 ? 'PM' : 'AM';
+
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  const hoursStr = String(hours).padStart(2, '0');
+
+  return `${day}/${month}/${year} ${hoursStr}:${minutes} ${ampm}`;
+};
